ec24f8d22f803a6e62f8e99d7d90cdc11967a500
GabsMermaid <g_martinez_rodz@gmail.com>
2022-02-11 20:55:17 +0000

    Adding js file for task 1

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,3 +1,15 @@
-$(".overdue").each( function() {
-    alert("Your book is overdue.");
+$(function (){
+	const amenityDict = {}
+	$(':checkbox'.change(function() {
+		if(this.checked) {
+			amentityDict[$(this).data('id')] = $(this).data('name');
+		} else {
+			delete amenityDict[$(this).data('id')];
+		}
+		if ($.isEmptyObject(amenityDict)) {
+			$('.amenities h4').html('&nbsp');
+		} else {
+			$('.amenities h4').text(Object.values(amenityDict).join(', '));
+		}
+	});
 });
