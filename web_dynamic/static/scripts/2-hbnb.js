db808cef835f692428914785d7d6af9826996e7e
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-02-12 02:51:20 +0000

    Fix typo in status function

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -17,9 +17,9 @@ $(function () {
 
 $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
 	if (data.status === 'OK') {
-		$(div#api_status).addClass('available');
+		$('div#api_status').addClass('available');
 	}
 	else {
-		$(div#api_status).removeClass('available');
+		$('div#api_status').removeClass('available');
 	}
 });
