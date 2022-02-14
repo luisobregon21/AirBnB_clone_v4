0a5d3568269e0413042ca2312e05e4058ab8ff3d
GabsMermaid <g_martinez_rodz@gmail.com>
2022-02-14 03:52:52 +0000

    Updating task 4 js script

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -42,16 +42,14 @@ $('button').click(function () {
 	$.ajax({
 		type: 'POST',
 		url: 'http://0.0.0.0:5001/api/v1/places_search',
-		data: { 
-			id: $(this).val()
-			access_token: $("").val()}
-		success: function (result){
-			alert('ok');
-		},
-		error: funtion (result){
-			alert ('error');
-			}
+		contentType: 'application/json',
+		dataType: 'json',
+		data: JSON.stringify({ amenities: Object.keys(amens) }),
+		success: function (data){
+			$('article').remove();
+			for (const place of Object.values(data)) {
+				$('.places ').append('<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
+			},
 		}
 	}
 });
-
